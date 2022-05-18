eadc75f1bfa39936b5f91f0cb45b37d0cdc69ad0
Reactdv <104239643+Reactdv@users.noreply.github.com>
2022-05-19 05:36:53 +0800

    Add files via upload

---
+++ Screens/ColorBtn.js
@@ -0,0 +1,21 @@
+import React from "react"
+import {StyleSheet,Text} from "react-native"
+
+
+
+
+
+
+
+function ColorButton (){
+  
+  return (
+    <View>
+   <Text> 
+   
+   textcndj
+   </Text> 
+   </View>
+    )
+}
+export default ColorButton;
